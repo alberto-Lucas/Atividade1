5e5ec73e9eefa9bb86c697dfc27d9d76a3ba4c5f
Lucas Alberto <lucas.alberto@linx.com.br>
2022-08-19 19:38:40 -0300

    atividade1

---
+++ Atividade1/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Atividade1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Clicked(object sender, EventArgs e)
+        {
+            if (ValidarLogin(txtlogin.Text, txtSenha.Text))
+            {
+                var usuarioLogado = UsuarioLogado.Instancia;
+                usuarioLogado.Login = txtlogin.Text;
+
+                Navigation.PushModalAsync(new Principal());
+            }
+            else
+                DisplayAlert("Atenção",
+                            "Login ou Senha inválidos!",
+                            "Ok");
+        }
+
+        bool ValidarLogin(string login, string senha)
+        {
+            return (login == "admin" && senha == "admin");
+        }
+    }
+}
---
+++ Atividade1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Atividade1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Cadastro", "Nome: " + txtNome.Text +
+                                     "\nIdade: " + txtIdade.Text +
+                                     "\nSexo: " + txtSexo.Text +
+                                     "\nTelefone: " + txtTelefone.Text,
+                                     "Ok");
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtIdade.Text = "";
+            txtSexo.Text = "";
+            txtTelefone.Text = "";
+        }
+    }
+}
